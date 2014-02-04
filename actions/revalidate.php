0a81dcb3909813b818c415d738769f2df827b1fb
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-02-04 11:12:44 +0100

    fixed usage of deprecated function

--- actions/revalidate.php
+++ actions/revalidate.php
@@ -6,7 +6,7 @@ $email = $user->getPrivateSetting("pleio_official_validator_email");
 $code = $user->getPrivateSetting("pleio_official_validator_code");
 
 if (!empty($email) && !empty($code)) {
-	if (pleio_official_validator_send_validation_mail($email, $code, get_loggedin_userid(), true)) {
+	if (pleio_official_validator_send_validation_mail($email, $code, elgg_get_logged_in_user_guid(), true)) {
 		system_message(elgg_echo("pleio_official_validator:action:revalidate:success"));
 	} else {
 		register_error(elgg_echo("pleio_official_validator:action:revalidate:error:save"));
